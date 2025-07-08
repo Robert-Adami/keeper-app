14196c62542088281e2f615631e5d61f7c716b40
Robert Adami <robertadami@Roberts-MacBook-Air.local>
2025-07-08 07:39:18 +0200

    Added some styling and animations

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,10 +1,37 @@
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+import { useState } from "react";
+
 function CreateArea({ handler, titleValue, areaValue, submitter }) {
+  const [boolean, setBoolean] = useState(false);
+  function handleBoolean() {
+    setBoolean(true);
+  }
   return (
     <div>
-      <form>
-        <input name="noteTitle" placeholder="Title" onChange={handler} value={titleValue}/>
-        <textarea name="noteContent" placeholder="Take a note..." rows="3" onChange={handler} value={areaValue}/>
-        <button onClick={submitter}>Add</button>
+      <form className="create-note">
+        {boolean ? (
+          <input
+            name="noteTitle"
+            placeholder={boolean === true ? "Title" : " "}
+            onChange={handler}
+            value={titleValue}
+          />
+        ) : null}
+        <textarea
+          name="noteContent"
+          placeholder="Take a note..."
+          rows={boolean ? "3" : "1"}
+          onChange={handler}
+          value={areaValue}
+          onClick={handleBoolean}
+        />
+        <Zoom in={boolean}>
+          <Fab onClick={submitter}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,9 +1,11 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+
 function Note({ title, content, deleter, id}) {
   return (
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
-      <button onClick={() => deleter(id)}>DELETE</button>
+      <button onClick={() => deleter(id)}><DeleteIcon/></button>
     </div>
   );
 }
